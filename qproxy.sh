f0996fad39878e749283a21ed8019fba525eea72
Dmitry Malkov <nickfailure@gmail.com>
2022-06-04 15:30:41 +0300

    Added support for HTTP proxy (Squid). Added normal processing of command line parameters. And most importantly, the colors.

--- qproxy.sh
+++ qproxy.sh
@@ -1,24 +1,79 @@
 #!/bin/env bash
 
-# Proxy port. Third argument or default value.
-PORT=${3:-"5678"}
+SocksDefaultPort="5678"
+HttpDefaultPort="5679"
 
-if [ ! $1 ] || [ ! $2 ] || [ "$1" == "-h" ] || [ "$1" == "--help" ]
-then
-	echo "Usage: $0 user password [port] ['orig']"
-	exit 1
+InfoColor="32"
+ErrorColor="31"
+DoneColor="1;33"
+
+function cecho ()
+{
+    echo -e "\e[$1m$2\e[0m"
+}
+
+function show_help ()
+{
+    cat <<EOF
+Usage: $0 [options] user password
+
+    -s          Install SOCKS proxy
+    -t          Install HTTP proxy
+
+    -S port     SOCKS proxy port
+    -T port     HTTP proxy port
+
+    -w          Emulate Windows TCP fingerprint
+
+    -h          Show help
+EOF
+    exit 0
+}
+
+UseSocks=""
+UseHttp=""
+MaskFP=""
+
+SocksPort=$SocksDefaultPort
+HttpPort=$HttpDefaultPort
+
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+while getopts "h?stS:?T:?w" opt; do
+  case "$opt" in
+    h|\?) show_help ;;
+    s)  UseSocks="yes" ;;
+    t)  UseHttp="yes" ;;
+    S)  SocksPort=$OPTARG ;;
+    T)  HttpPort=$OPTARG ;;
+    w) MaskFP="yes" ;;
+  esac
+done
+
+shift $((OPTIND-1))
+
+
+ProxyUser=$1
+ProxyPassword=$2
+
+if [ ! $ProxyUser ] || [ ! $ProxyPassword ]; then
+    show_help
 fi
 
-USER=$1
-PASSWORD=$2
+if [ -z $UseSocks ] && [ -z $UseHttp ]; then
+    cecho $ErrorColor "You have to specify the -s or -t option, then I'll do something for you"
+    show_help
+fi
 
-DANTE_CONFIG=$(cat <<EOF
+
+DanteConfig=$(cat <<EOF
 logoutput: syslog
 user.privileged: root
 user.unprivileged: nobody
 
 # The listening network interface or address.
-internal: 0.0.0.0 port=$PORT
+internal: 0.0.0.0 port=$SocksPort
 
 # The proxying network interface or address.
 external: eth0
@@ -37,9 +92,21 @@ socks pass {
     from: 0.0.0.0/0 to: 0.0.0.0/0
 }
 EOF
-)
+            )
 
-SYSCTL_CONF=$(cat <<'EOF'
+SquidConfig=$(cat <<EOF
+
+http_port $HttpPort
+
+auth_param basic program /usr/lib/squid3/basic_ncsa_auth /etc/squid/passwords
+auth_param basic realm proxy
+acl authenticated proxy_auth REQUIRED
+
+http_access allow authenticated
+EOF
+           )
+
+SysctlConfig=$(cat <<'EOF'
 net.ipv4.ip_default_ttl = 128
 net.ipv4.route.min_adv_mss = 1460
 net.ipv4.tcp_rmem = 8192 87380 4194304
@@ -50,27 +117,43 @@ net.ipv4.tcp_window_scaling=0
 EOF
 )
 
-echo "Installing dante-server..."
-sudo apt update
-sudo apt install dante-server
+if [ $UseSocks ]; then
+    cecho $InfoColor "Installing Dante..."
+    sudo apt update
+    sudo apt install dante-server
 
-echo "Updating config files..."
-sudo mv /etc/danted.conf /etc/danted.conf.orig
-sudo bash -c  "echo \"$DANTE_CONFIG\" > /etc/danted.conf"
+    cecho $InfoColor "Updating Dante config file..."
+    sudo mv /etc/danted.conf /etc/danted.conf.orig
+    sudo bash -c  "echo \"$DanteConfig\" > /etc/danted.conf"
 
-echo "Adding user $USER..."
-sudo useradd -r -s /bin/false $USER
-echo "$USER:$PASSWORD" | sudo chpasswd
+    cecho $InfoColor "Adding system user $ProxyUser..."
+    sudo useradd -r -s /bin/false $ProxyUser
+    echo "$ProxyUser:$ProxyPassword" | sudo chpasswd
 
-echo "Restarting Dante..."
-sudo systemctl restart danted.service
-sudo systemctl show danted.service | fgrep "ActiveState"
-
-if [ "$4" != "orig" ]
-then
-	echo "Setting up Windows TCP fingerprint..."
-	sudo bash -c "echo \"$SYSCTL_CONF\" > /etc/sysctl.d/01-win-finger.conf"
-	sudo sysctl -p /etc/sysctl.d/01-win-finger.conf > /dev/null
+    cecho $InfoColor "Restarting Dante..."
+    sudo systemctl restart danted.service
+    sudo systemctl show danted.service | fgrep "ActiveState"
 fi
 
-echo "DONE"
+if [ $UseHttp ]; then
+    cecho $InfoColor "Installing Squid..."
+    sudo apt install squid apache2-utils
+
+    cecho $InfoColor "Updating Squid config file..."
+    sudo bash -c "echo \"$SquidConfig\" >> /etc/squid/squid.conf"
+
+    cecho $InfoColor "Adding Squid user $ProxyUser..."
+    echo $ProxyPassword | sudo htpasswd -i -c /etc/squid/passwords $ProxyUser
+
+    cecho $InfoColor "Restarting Squid..."
+    sudo systemctl restart squid.service
+    sudo systemctl show squid.service | fgrep "ActiveState"
+fi
+
+if [ $MaskFP ]; then
+    cecho $InfoColor "Setting up Windows TCP fingerprint..."
+    sudo bash -c "echo \"$SYSCTL_CONF\" > /etc/sysctl.d/01-win-finger.conf"
+    sudo sysctl -p /etc/sysctl.d/01-win-finger.conf > /dev/null
+fi
+
+cecho $DoneColor "DONE"
